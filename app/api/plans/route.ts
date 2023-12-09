e478498d6470a51cfae2efd0b3489039b404cbac
kamal.swami <contej.business@gmail.com>
2023-12-09 20:28:15 +0530

    added plan models

--- app/api/plans/route.ts
+++ app/api/plans/route.ts
@@ -3,24 +3,23 @@ import { auth } from "@clerk/nextjs";
 
 import prismadb from "@/lib/prismadb";
 
+
 export async function POST(
   req: Request,
-  { params }: { params: { storeId: string } }
 ) {
   try {
-    const { userId } = auth();
 
     const body = await req.json();
 
-    const { name, price, reward } = body;
+    const {  name,
+    price,
+    reward } = body;
 
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 403 });
-    }
-
+   
     if (!name) {
       return new NextResponse("Name is required", { status: 400 });
     }
+
 
     if (!price) {
       return new NextResponse("Price is required", { status: 400 });
@@ -30,20 +29,6 @@ export async function POST(
       return new NextResponse("Category id is required", { status: 400 });
     }
 
-    if (!params.storeId) {
-      return new NextResponse("Store id is required", { status: 400 });
-    }
-
-    const storeByUserId = await prismadb.store.findFirst({
-      where: {
-        id: params.storeId,
-        userId,
-      },
-    });
-
-    if (!storeByUserId) {
-      return new NextResponse("Unauthorized", { status: 405 });
-    }
 
     const plan = await prismadb.plan.create({
       data: {
@@ -52,23 +37,18 @@ export async function POST(
         reward,
       },
     });
-
+  
     return NextResponse.json(plan);
   } catch (error) {
-    console.log("[PLANS_POST]", error);
+    console.log('[PLANS_POST]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
-}
+};
 
-export async function GET(
-  req: Request,
-  { params }: { params: { storeId: string } }
-) {
+
+
+export async function GET(req: Request) {
   try {
-    if (!params.storeId) {
-      return new NextResponse("Store id is required", { status: 400 });
-    }
-
     const plans = await prismadb.plan.findMany({
       orderBy: {
         createdAt: "desc",
